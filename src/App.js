648c55d3b7c4c388e30ff21eb8b8ce3e892aa360
pandit <dhamdherepandit@gmail.com>
2023-01-08 15:51:04 +0530

    started styling app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import coldBg from './assets/cold.jpg'
 
 function App() {
   return (
-   <div className="app">
-    <h1>hello Pandit</h1>
-   </div>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+        <div className="container">
+          <div className="section section__inputs">
+            <input type="text" name="city" placeholder="Enter the city name" />
+            <button>°F</button>
+
+            <div>
+              <div className="section__tempreture">
+                <div className="icon">
+                  <h3>Pune</h3>
+                  <img src="" alt="" />
+                  <h3>Cloudy</h3>
+                </div>
+                <div className="tempreture">
+                  <h1>25°C</h1>
+                </div>
+              </div>
+
+              {/*bottom description */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
