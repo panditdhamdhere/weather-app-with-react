512e65209fb66d210877b819a8a237c89ce1a19d
pandit <dhamdherepandit@gmail.com>
2023-01-08 21:56:01 +0530

    fixeed css bug

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ function App() {
       <div className="overlay">
         <div className="container">
           <div className="section section__inputs">
-            <input type="text" name="city" placeholder="Enter the city name" />
+            <input type="text" name="city" placeholder="Enter city" />
             <button>°F</button>
+          </div>
 
-            <div>
-              <div className="section__tempreture">
-                <div className="icon">
-                  <h3>Pune</h3>
-                  <img src="" alt="" />
-                  <h3>Cloudy</h3>
-                </div>
-                <div className="tempreture">
-                  <h1>25°C</h1>
-                </div>
-              </div>
+          <div className="section section__tempreture">
+            <div className="icon">
+              <h3>London</h3>
+              <img src="" alt="" />
+              <h3>cloudy</h3>
+            </div>
 
-              {/*bottom description */}
+            <div className="tempreture">
+              <h1>25° C</h1>
             </div>
           </div>
+
+          {/* bootom description*/}
         </div>
       </div>
     </div>
+      
   );
 }
 
