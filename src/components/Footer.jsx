94323f93a3b4fc84c349e44f8524a864077abac6
pandit <dhamdherepandit@gmail.com>
2023-01-11 09:54:35 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <p>made with love by pandit</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
